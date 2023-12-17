878826afb035af42a6340d9004f2697c0248d2e3
Latish Sherigar <latishsherigar@gmail.com>
2023-12-17 23:28:08 +0530

    about

---
+++ gascript.js
@@ -0,0 +1,6 @@
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+var pageTracker = _gat._getTracker("UA-4577935-1");
+pageTracker._initData();
+pageTracker._trackPageview();
+
